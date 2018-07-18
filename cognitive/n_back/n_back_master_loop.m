46dc635399be18d3cf0775fee331be5bc4ee385c
Joshua Hendrikse <joshua.hendrikse@monash.edu>
2018-07-18 17:46:29 +1000

    Changed script descriptions

--- cognitive/n_back/n_back_master_loop.m
+++ cognitive/n_back/n_back_master_loop.m
@@ -1,13 +1,13 @@
 clear; close all; clc;
-% This script runs a for loop to output the main outcome measures for the
+
+% This script can be used to return the main outcome measures for the
 % n-back task across the ex rTMS participant sample 
 
 ActiveID = {'S2_MS';'S3_DJ';'S4_JM';'S5_RD';'S6_KV';'S8_AW';'S9_SF';'S10_JT';'S11_RB';'S16_YS';'S17_JTR';'S19_JA';'S20_WO';'S22_NS';'S25_SC';'S27_ANW';'S33_DJG';'S34_ST';'S35_TG';'S36_AY'};
 
 InactiveID = {'S7_PK';'S13_MD';'S15_AZ';'S18_KF';'S21_KC';'S24_AU';'S26_KW';'S28_XK';'S29_HZ';'S30_PKA';'S31_AR';'S32_CD';'S37_JT';'S38_CR';'S39_EH';'S40_NU';'S41_JC';'S42_SA';'S43_PL';'S44_ID'};
 
-% Condition = {'wk1_llpc';'wk2_sma'} - This was replaced for pathIn_mod ln
-% 19 - allowing use of wildcard for stimulation conditions 
+% Condition = {'wk1_llpc';'wk2_sma'} - This was replaced for pathIn_mod - allowing use of wildcard character for stimulation conditions 
 
 timePoint = {'pre';'post';'follow_up'};
 
