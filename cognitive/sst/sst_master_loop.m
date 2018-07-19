0071da7f23b7de83e131096fb8c5952b7815e43c
Joshua Hendrikse <joshua.hendrikse@monash.edu>
2018-07-19 19:36:44 +1000

    Altered task description and participant ID strings

--- cognitive/sst/sst_master_loop.m
+++ cognitive/sst/sst_master_loop.m
@@ -4,16 +4,16 @@ clear; close all; clc;
 % stop-signal task across the ex rTMS participant sample 
 
 
-ActiveID = {'S2_MS';'S3_DJ';'S4_JM';'S5_RD';'S6_KV';'S8_AW';'S9_SF';'S10_JT';'S11_RB';'S16_YS';'S17_YS';'S19_JA';'S20_WO';'S22_NS';'S25_SC';'S27_ANW'};
+ActiveID = {'S2_MS';'S3_DJ';'S4_JM';'S5_RD';'S6_KV';'S8_AW';'S9_SF';'S10_JT';'S11_RB';'S16_YS';'S17_YS';'S19_JA';'S20_WO';'S22_NS';'S25_SC';'S27_ANW';'S34_ST';'S35_TG';'S36_AY'};
 
-InactiveID = {'S1_JC';'S7_PK';'S13_MD';'S15_AZ';'S18_KF';'S21_KC';'S24_AU';'S26_KW';'S28_XK';'S29_HZ';'S30_PKA';'S31_AR';'S32_CD'};
+InactiveID = {'S1_JC';'S7_PK';'S13_MD';'S15_AZ';'S18_KF';'S21_KC';'S24_AU';'S26_KW';'S28_XK';'S29_HZ';'S30_PKA';'S31_AR';'S32_CD';'S37_JT';'S38_CR';'S39_EH';'S40_NU';'S41_JC';'S42_SA';'S43_PL';'S44_ID'};
 
 timePoint = {'pre';'post';'follow_up'};
 
 % Condition = {'wk1_llpc';'wk2_sma'} - This was replaced for pathIn_mod ln
 % 19 - allowing use of wildcard for stimulation conditions 
 
-pathIn = '/Volumes/GoogleDrive/My Drive/Data_backup/Data_ex_rTMS';
+pathIn = '/Volumes/Lacie/Ex_rTMS_study/Data';
 
 %% loop over subjects in active group
 
@@ -27,7 +27,7 @@ for x = 1:length(ActiveID)
         
         for z = 1:length(timePoint)
             
-            filename_SST = [char(pathIn),'/Active/',char(ActiveID(x,1)),'/Cognitive/',char(Condition(y,1)),'/SST/',char(timePoint(z,1)),'/','SST_',char(ActiveID(x,1)),'_',char(timePoint(z,1)),'.dat']
+            filename_SST = [char(pathIn),'/Active/',char(ActiveID(x,1)),'/Cognitive/',char(Condition(y,1)),'/SST/',char(timePoint(z,1)),'/',char(ActiveID(x,1)),'_',char(timePoint(z,1)),'.dat']
             
             % filename = [pathIn,'/','Active','/',ActiveID(x,1),'/','Cognitive','/',Condition(y,1),'/','SST','/',timePoint(z,1),'/','SST_',ActiveID(x,1),'_',timePoint(z,1),'.dat'] 
             
