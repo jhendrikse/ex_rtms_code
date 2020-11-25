b6cd48115705197bd700cd2844f7e7f1cad9a1a5
Joshua Hendrikse <joshua.hendrikse@monash.edu>
2020-11-25 21:58:41 +1100

    Changes to sst based on elife consensus paper * go rt omissions replaced with highest go rt * minor changes to mean ssd calculation

--- cognitive/sst/sst_master.m
+++ cognitive/sst/sst_master.m
@@ -1,21 +1,39 @@
-function [ssrt_integration, ssrt_mean, mean_rt_go_corr, mean_rt_go_uncorr, corr_prob_stop_trial, incorr_prob_stop_trial] = sst_master(filename_SST)
+function [mean_ex_gauss,sigma_ex_gauss,tau_ex_gauss,X,chiSquare,ssrt_integration,ssrt_mean] = sst_master(filename_SST)
 
 % This function can be used to analyse the BMH version of the stop-signal task, and outputs the mean reaction time for correct go trials, and the stop signal response time according to both the integration and mean methods, 
 ... as specified in Verbruggen, F., Chambers, C. D., & Logan, G. D. (2013). Fictitious inhibitory differences: how skewness and slowing distort the estimation of stopping latencies. Psychological Science, 24, 352?362.
 
 data = importfile_SST(filename_SST);
 
-%% calculate mean RT on go trials 
+%% calculate RTs on go trials 
 
 go_trial = data.valuessignal == 0; %create logical for go trials
 rt_go_trial = data.valuesrt(go_trial); %extract go trial reaction times
-mean_rt_go_uncorr = mean(rt_go_trial); %calculate mean rt on all go trials 
-rt_go_error_hbound = rt_go_trial > (mean(rt_go_trial) + (2.5.*std(rt_go_trial))) ; %create logical for go trial rt's above mean + 2SD's
-rt_go_error_lbound = rt_go_trial < (mean(rt_go_trial) - (2.5.*std(rt_go_trial))) ; %create logical for go trial rt's below mean - 2SD's
-rt_go_error_bound_all = rt_go_error_hbound + rt_go_error_lbound;
-rt_go_corr = rt_go_trial(rt_go_error_bound_all ~= 1); %extract go trials without errors
-mean_rt_go_corr = mean(rt_go_corr); %calculate mean rt on go trials without errors
 
+go_trial_omissions_log = rt_go_trial == 0 ; %create logical for go trial omissions
+
+if any(go_trial_omissions_log,'all') % check if go rt's contain omissions (RT's = 0) 
+    go_rt_max = max(rt_go_trial) ;% calculate max rt for go trials
+    rt_go_omission_replace = rt_go_trial ; % create new variable for go rt's w/ replacement
+    rt_go_omission_replace(go_trial_omissions_log) = go_rt_max ; % for go rt omissions, replace with max go rt
+else 
+    rt_go_omission_replace = rt_go_trial ; % otherwise, if no rt omissions, variable is equal to original go rt distribution 
+end
+
+rt_go_omission_count = sum(double(go_trial_omissions_log)) ; % calculate number of ommission errors (i.e. go trials where no rt was logged)
+rt_go_commission_error = data.valuesrt(data.valuessignal == 0 & (data.valuesstimulus ~= data.valuesresponse)) % extract go rt's for incorrect go trials (i.e. commission errors)
+rt_go_commission_count = numel(rt_go_commission_error) ; % calculate number of commission errors on go trials (i.e. incorrectly responding to direction of arrow)
+
+mean_rt_go = mean(rt_go_trial); %calculate mean rt on all go trials (without replacement for omissions)
+mean_rt_go_omission_replace = mean(rt_go_omission_replace) ; % calculate mean rt on go trials with replacement of maximum rt 
+
+%check reaction time distribution when RT's above and below 2.5 sd are
+%removed 
+% rt_go_error_hbound = rt_go_trial > (mean(rt_go_trial) + (2.5.*std(rt_go_trial))) ; %create logical for go trial rt's above mean + 2SD's
+% rt_go_error_lbound = rt_go_trial < (mean(rt_go_trial) - (2.5.*std(rt_go_trial))) ; %create logical for go trial rt's below mean - 2SD's
+% rt_go_error_bound_all = rt_go_error_hbound + rt_go_error_lbound;
+% rt_go_corr = rt_go_trial(rt_go_error_bound_all ~= 1); %extract go trials without errors
+% mean_rt_go_corr = mean(rt_go_corr); %calculate mean rt on go trials without errors
 
 %% Exgauss modelling using Bram Zandbelt's code
 %Obtain best-fitting ex-Gaussian parameters (X) by fitting the ex-Gaussian
@@ -29,48 +47,61 @@ rt_go_trial_2 = rt_go_trial(rt_go_trial > 150);
 % rt_go_trial_2 (i.e. y)  - Nx1 vector of observed response times
 % X             - 1x3 best-fitting parameter values (mu,sigma,tau)
 % fVal          - negative log-likelihood (-fVal is log-likelihood)
+
 [X,fVal,exitFlag,solverOutput] = exgauss_fit(rt_go_trial_2);
+
+% extract individual ex gauss parameters
+mean_ex_gauss = X(1,1) ;
+sigma_ex_gauss = X(1,2) ;
+tau_ex_gauss = X(1,3) ;
 
 % Computes the chi-square statistic
 chiSquare = exgauss_chi_square(rt_go_trial_2,X);
-
-% Subplot of pdf and cdf with large 150ms-1000ms x-axis RT range 
-figure;hold on
-set(gcf, 'Units', 'Normalized', 'OuterPosition', [0.02, 0.02, 0.6, 0.9]); % Enlarge figure to full screen.
-
-subplot(3,2,1), exgauss_plot('pdf',rt_go_trial_2,X);
-axis([150 1000 0 50]);
-
-subplot(3,2,2), exgauss_plot('cdf',rt_go_trial_2,X);
-axis([150 1000 0 1]);
-
-%Plot both cdf and pdf functions on smaller window
-%exgauss_plot('both',rt_go_trial_2,X);
+% 
+% %Subplot of pdf and cdf with large 150ms-1000ms x-axis RT range 
+% figure;hold on
+% set(gcf, 'Units', 'Normalized', 'OuterPosition', [0.02, 0.02, 0.6, 0.9]); % Enlarge figure to full screen.
+% 
+% subplot(3,2,1), exgauss_plot('pdf',rt_go_trial_2,X);
+% axis([150 1000 0 50]);
+% 
+% subplot(3,2,2), exgauss_plot('cdf',rt_go_trial_2,X);
+% axis([150 1000 0 1]);
+% 
+% %Plot both cdf and pdf functions on smaller window
+% exgauss_plot('both',rt_go_trial_2,X);
 
 %next steps - save out this information in group level summary script - use parameters
 %for QC
 %% calculate stop-signal response time
 
 total_ss_trial = sum(data.valuessignal); % calculate number of stop signals presented
-total_go_trial = length(rt_go_trial); %calculate total number of go trials
-total_go_trial_corr = length(rt_go_corr); %calculate total number of go trials, errors removed. 
+% total_go_trial = length(rt_go_trial); %calculate total number of go trials
+% total_go_trial_corr = length(rt_go_corr); %calculate total number of go trials, errors removed. 
+total_go_trial = length(rt_go_omission_replace); %calculate total number of go trials, omissions replaced. 
 ssd_change = nonzeros(data.valuesssd); % remove zero values from stop-signal delay vector (i.e. those corresponding to go trials)
-mean_ssd = mean(ssd_change(2:end)); % remove first ssd which is set to a 250ms default starting value
-rorder_go_rt = sortrows(rt_go_corr); %rank order corrected go reaction times (i.e. outliers removed) with fastest response rank order 1
+%mean_ssd = mean(ssd_change(2:end)); % remove first ssd which is set to a 250ms default starting value
+mean_ssd = mean(ssd_change); % calculate mean ssd
+% rorder_go_rt = sortrows(rt_go_corr); %rank order corrected go reaction times (i.e. outliers removed) with fastest response rank order 1
+
+rorder_go_rt_omission_replace = sortrows(rt_go_omission_replace) ; %rank order go reaction times w/ omissions replaced with fastest response rank order 1
+
 logstop = data.valuessignal == 1; %create logical array for stop trials 
 incorr_stop_trial_rt = nonzeros(data.valuesrt(logstop)); % extract RT's for stop fails 
 mean_incorr_stop_trial_rt = mean(incorr_stop_trial_rt) ; % calculate mean RT's for stop fails 
 total_stop_trial_corr = (sum(double(data.valuescorrect(logstop)) == 2)); %calculate the number of correct stop trials
 total_stop_trial_incorr = (sum(double(data.valuescorrect(logstop)) == 0)); %calculate the number of incorrect stop trials
 corr_prob_stop_trial = total_stop_trial_corr / total_ss_trial ; %calculate the percentage of correct stop trials
-incorr_prob_stop_trial = total_stop_trial_incorr/ total_ss_trial ; %calculate the percentage of incorrect stop trials
-nth_index_delay = round((total_go_trial_corr .* incorr_prob_stop_trial),0); % multiplies the total number of go trials by the percentage of incorrect stop trials (i.e. p(respond|signal)) & rounds to closest integer
+p_respond_given_stop = total_stop_trial_incorr/ total_ss_trial ; %calculate the percentage of incorrect stop trials
+nth_index_delay = round((total_go_trial .* p_respond_given_stop),0); % multiplies the total number of go trials by the percentage of incorrect stop trials (i.e. p(respond|signal)) & rounds to closest integer
 
 % calculate ssrt values according to both integration and mean methods (if
 % performance on task has been adequate and resulted in ssrt above 0 (in task data)) 
+
+% may need to add other if expressions for QC checks
 if (data.expressionsssrt(length(data.expressionsssrt))) > 0
-    ssrt_integration = (rorder_go_rt(nth_index_delay)) - mean_ssd ;% ssrt w/integration method (nth go rt - mean ssd)
-    ssrt_mean = mean_rt_go_corr - mean_ssd ; % ssrt w/mean method
+    ssrt_integration = (rorder_go_rt_omission_replace(nth_index_delay)) - mean_ssd ;% ssrt w/integration method (nth go rt - mean ssd)
+    ssrt_mean = mean_rt_go - mean_ssd ; % ssrt w/mean method
 
 % otherwise return 99999 as missing value
 else
--- cognitive/sst/sst_master_loop_cross_over.m
+++ cognitive/sst/sst_master_loop_cross_over.m
@@ -2,7 +2,8 @@ clear; close all; clc;
 
 % This script returns the ssrt according to both integration and mean calculation methods for the
 % ICAB stop-signal task. The output is structured for within-subject
-% comparison of stimulation conditions for ex_rTMS sample.  
+% comparison of stimulation conditions for ex_rTMS sample. 
+% Joshua Hendrikse joshua.hendrikse@monash.edu
 
 ID = {'S2_MS';'S3_DJ';'S4_JM';'S5_RD';'S6_KV';'S7_PK';'S8_AW';'S9_SF';'S10_JT';'S11_RB';'S13_MD';'S15_AZ';'S16_YS';'S17_JTR';'S18_KF';'S19_JA';'S20_WO';'S21_KC';'S22_NS';'S24_AU';'S25_SC';'S26_KW';'S27_ANW';'S28_XK';'S29_HZ';'S30_PKA';'S31_AR';'S32_CD';'S33_DJG';'S34_ST';'S35_TG';'S36_AY';'S37_JT';'S38_CR';'S39_EH';'S40_NU';'S41_JC';'S42_SA';'S43_PL';'S44_ID'};
 
@@ -16,6 +17,7 @@ pathIn = '/Volumes/Lacie/Ex_rTMS_study/Data';
 
 Condition = {'llpc';'sma'};
 
+
 for x = 1:length(ID)
     
     for y = 1:length(Condition)
@@ -24,30 +26,87 @@ for x = 1:length(ID)
             
             filename_sst = [char(pathIn),'/all_subjects/',char(ID(x,1)),'/Cognitive/',char(Condition(y,1)),'/sst/',char(timePoint(z,1)),'/',char(ID(x,1)),'_',char(timePoint(z,1)),'.dat'];
             
-            [ssrt_integration(x,z,y),ssrt_mean(x,z,y)] = sst_master(filename_sst); %% stores output as 40 X 3 X 2 matrix. 
+            %[~,~,~,~,~,ssrt_integration(x,z,y),ssrt_mean(x,z,y)] = sst_master(filename_sst); %% stores only ssrt_integration, ssrt_mean values (40 X 3 X 2 matrix). 
+            
+            [mean_ex_gauss(x,z,y),sigma_ex_gauss(x,z,y),tau_ex_gauss(x,z,y),~,chiSquare(x,z,y),ssrt_integration(x,z,y),ssrt_mean(x,z,y)] = sst_master(filename_sst); %% stores Exgauss parameters & ssrt_integration, ssrt_mean as 40 X 3 X 2 matrix.
         end
     end
-   
 end
+
 
 % Separate timepoints and conditions for entry into table
 
-% learning score llpc
+% mean ex gauss llpc
+mean_ex_gauss_pre_llpc = mean_ex_gauss(:,1,1) ;
+mean_ex_gauss_post_llpc = mean_ex_gauss(:,2,1) ;
+mean_ex_gauss_follow_up_llpc = mean_ex_gauss(:,3,1) ;
+
+% mean ex gauss sma
+mean_ex_gauss_pre_sma = mean_ex_gauss(:,1,2) ; 
+mean_ex_gauss_post_sma = mean_ex_gauss(:,2,2) ; 
+mean_ex_gauss_follow_up_sma = mean_ex_gauss(:,3,2) ; 
+
+% sigma ex gauss llpc
+sigma_ex_gauss_pre_llpc = sigma_ex_gauss(:,1,1) ;
+sigma_ex_gauss_post_llpc = sigma_ex_gauss(:,2,1) ;
+sigma_ex_gauss_follow_up_llpc = sigma_ex_gauss(:,3,1) ;
+
+% sigma ex gauss sma
+sigma_ex_gauss_pre_sma = sigma_ex_gauss(:,1,2) ; 
+sigma_ex_gauss_post_sma = sigma_ex_gauss(:,2,2) ; 
+sigma_ex_gauss_follow_up_sma = sigma_ex_gauss(:,3,2) ; 
+
+% tau ex gauss llpc
+tau_ex_gauss_pre_llpc = tau_ex_gauss(:,1,1) ;
+tau_ex_gauss_post_llpc = tau_ex_gauss(:,2,1) ;
+tau_ex_gauss_follow_up_llpc = tau_ex_gauss(:,3,1) ;
+
+% tau ex gauss sma
+tau_ex_gauss_pre_sma = tau_ex_gauss(:,1,2) ; 
+tau_ex_gauss_post_sma = tau_ex_gauss(:,2,2) ; 
+tau_ex_gauss_follow_up_sma = tau_ex_gauss(:,3,2) ; 
+
+% Chi squared llpc
+chiSquare_pre_llpc = chiSquare(:,1,1) ;
+chiSquare_post_llpc = chiSquare(:,2,1) ;
+chiSquare_follow_up_llpc = chiSquare(:,3,1) ;
+
+% Chi squared sma
+chiSquare_pre_sma = chiSquare(:,1,2) ; 
+chiSquare_post_sma = chiSquare(:,2,2) ; 
+chiSquare_follow_up_sma = chiSquare(:,3,2) ;
+
+% ssrt_integration llpc
 ssrt_integration_pre_llpc = ssrt_integration(:,1,1) ;
 ssrt_integration_post_llpc = ssrt_integration(:,2,1) ;
 ssrt_integration_follow_up_llpc = ssrt_integration(:,3,1) ;
 
-% learning score sma
+% ssrt_integration sma
 ssrt_integration_pre_sma = ssrt_integration(:,1,2) ;
 ssrt_integration_post_sma = ssrt_integration(:,2,2) ;
 ssrt_integration_follow_up_sma = ssrt_integration(:,3,2) ;
 
+% ssrt_mean llpc
+ssrt_mean_pre_llpc = ssrt_mean(:,1,1) ;
+ssrt_mean_post_llpc = ssrt_mean(:,2,1) ;
+ssrt_mean_follow_up_llpc = ssrt_mean(:,3,1) ;
+
+% ssrt_mean sma
+ssrt_mean_pre_sma = ssrt_mean(:,1,2) ;
+ssrt_mean_post_sma = ssrt_mean(:,2,2) ;
+ssrt_mean_follow_up_sma = ssrt_mean(:,3,2) ;
+
 % Dummy variable to encode PA group (1 = active, 2 = sedentary)
 activity_group = [1;1;1;1;1;2;1;1;1;1;2;2;1;1;2;1;1;2;1;2;1;2;1;2;2;2;2;2;1;1;1;1;2;2;2;2;2;2;2;2] ; 
 
-Dataset_sst_all_subjects = table(ID,activity_group,ssrt_integration_pre_llpc,ssrt_integration_post_llpc,ssrt_integration_follow_up_llpc,ssrt_integration_pre_sma,ssrt_integration_post_sma,ssrt_integration_follow_up_sma) ; 
+Dataset_ssrt_all_subjects = table(ID,activity_group,ssrt_integration_pre_llpc,ssrt_integration_post_llpc,ssrt_integration_follow_up_llpc,ssrt_integration_pre_sma,ssrt_integration_post_sma,ssrt_integration_follow_up_sma,ssrt_mean_pre_llpc,ssrt_mean_post_llpc,ssrt_mean_follow_up_llpc,ssrt_mean_pre_sma,ssrt_mean_post_sma,ssrt_mean_follow_up_sma) ; 
 
-save('sst_output_cross_over.mat','Dataset_sst_all_subjects') 
-writetable(Dataset_sst_all_subjects,'Dataset_sst_all_subjects.xlsx','WriteRowNames',true) ;
+Dataset_ex_gauss_sst_all_subjects = table(ID,activity_group,mean_ex_gauss_pre_llpc,mean_ex_gauss_post_llpc,mean_ex_gauss_follow_up_llpc,mean_ex_gauss_pre_sma,mean_ex_gauss_post_sma,mean_ex_gauss_follow_up_sma,...
+    sigma_ex_gauss_pre_llpc,sigma_ex_gauss_post_llpc,sigma_ex_gauss_follow_up_llpc,sigma_ex_gauss_pre_sma,sigma_ex_gauss_post_sma,sigma_ex_gauss_follow_up_sma,tau_ex_gauss_pre_llpc,tau_ex_gauss_post_llpc,tau_ex_gauss_follow_up_llpc,...
+    tau_ex_gauss_pre_sma,tau_ex_gauss_post_sma,tau_ex_gauss_follow_up_sma,chiSquare_pre_llpc,chiSquare_post_llpc,chiSquare_follow_up_llpc,chiSquare_pre_sma,chiSquare_post_sma,chiSquare_follow_up_sma) ;
+    
+save('sst_output_cross_over.mat','Dataset_ssrt_all_subjects','Dataset_ex_gauss_sst_all_subjects') 
+writetable(Dataset_ssrt_all_subjects,'Dataset_ssrt_all_subjects.xlsx','WriteRowNames',true) ;
+writetable(Dataset_ex_gauss_sst_all_subjects,'Dataset_ex_gauss_sst_all_subjects.xlsx','WriteRowNames',true) ;
 movefile sst_output_cross_over.mat /Volumes/LaCie/Ex_rTMS_study/Data/Analysis/Datasets/ ;
 movefile Dataset_sst_all_subjects.xlsx /Volumes/LaCie/Ex_rTMS_study/Data/Analysis/Datasets/ ;
